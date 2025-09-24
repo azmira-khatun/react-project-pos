fa211d3866e2e0c91fd346f033c5fabc55f24daf
azmira-khatun <azmiragrn@gmail.com>
2025-09-24 11:39:55 +0600

    first commit

--- admin-panel/src/components/Sidebar.jsx
+++ admin-panel/src/components/Sidebar.jsx
@@ -4,16 +4,16 @@ const Sidebar = () => {
   return (
     <div>
       {/* Main Sidebar Container */}
-      <aside className="main-sidebar sidebar-dark-primary elevation-4">
+      <aside className="main-sidebar sidebar-light-primary elevation-4">
         {/* Brand Logo */}
         <a href="index3.html" className="brand-link">
           <img
-            src="assets-admin/dist/img/AdminLTELogo.png"
+            src="assets-admin/dist/img/pages-makna-logo-2.webp"
             alt="AdminLTE Logo"
             className="brand-image img-circle elevation-3"
             style={{ opacity: ".8" }}
           />
-          <span className="brand-text font-weight-light">AdminLTE 3</span>
+          <span className="brand-text font-weight-light">DREAM POS</span>
         </a>
         {/* Sidebar */}
         <div className="sidebar">
@@ -21,16 +21,16 @@ const Sidebar = () => {
           <div className="user-panel mt-3 pb-3 mb-3 d-flex">
             <div className="image">
               <img
-                src="assets-admin/dist/img/user2-160x160.jpg"
+                src="assets-admin/dist/img/avatar2.png"
                 className="img-circle elevation-2"
                 alt="User Image"
               />
             </div>
-            {/* <div className="info">
+            <div className="info">
               <a href="#" className="d-block">
-                Alexander Pierce
+                Azmira Khatun
               </a>
-            </div> */}
+            </div>
           </div>
           {/* SidebarSearch Form */}
           <div className="form-inline">
--- admin-panel/src/pages/category/ManageCategory.jsx
+++ admin-panel/src/pages/category/ManageCategory.jsx
@@ -35,7 +35,57 @@ const ManageCategory = () => {
           <section className="content">
             {/* Default box */}
             <div className="card">
-              <div className="card-body">Manage category page </div>
+              <div className="card-body">
+                {/* /.card-header */}
+                <div className="card-body table-responsive p-0">
+                  <table className="table table-hover text-nowrap">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>User</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>183</td>
+                        <td>John Doe</td>
+                        <td>11-7-2014</td>
+                        <td>
+                          <span className="tag tag-success">Approved</span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>219</td>
+                        <td>Alexander Pierce</td>
+                        <td>11-7-2014</td>
+                        <td>
+                          <span className="tag tag-warning">Pending</span>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>657</td>
+                        <td>Bob Doe</td>
+                        <td>11-7-2014</td>
+
+                        <td>
+                          Bacon ipsum dolor sit amet salami venison chicken
+                          flank fatback doner.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>175</td>
+                        <td>Mike Doe</td>
+                        <td>11-7-2014</td>
+                        <td>
+                          <span className="tag tag-danger">Denied</span>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
               {/* /.card-body */}
             </div>
             {/* /.card */}
